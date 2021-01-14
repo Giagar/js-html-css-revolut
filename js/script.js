c29e589584a6ba57f1ee7a818842a5c800ed71a0
Gianpaolo <pao.barga@gmail.com>
2021-01-14 17:23:09 +0100

    esercizio fatto (meccanismo di base)

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    //funziona, ma ogni volta che clicco sul menu dropdown, questo sparisce
+    // $("nav .dropdown-inside").click(function() {
+    //     $(this).find(".dropdown-menu").toggle();
+    // })
+    
+    //in questo modo il problema di prima non si presenta
+    $("nav .dropdown-inside span").click(function() {
+        $(this).parent().find(".dropdown-menu").toggle();
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
